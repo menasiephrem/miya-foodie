a1d08145f3bc935b6b7afd5c8aefd51c9bc7ce51
Menasi Ephrem <menasiephrem@gmail.com>
2021-02-03 16:53:49 +0300

    add home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/pic1.jpeg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: 
+           Container(
+            width: MediaQuery.of(context).size.width, 
+            padding: EdgeInsets.symmetric(vertical: 80),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Good Food",
+                      style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      "Deliciously Simple.",
+                      style: TextStyle(fontSize: 13),
+                    )
+                  ],
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.brown[300])),
+                  onPressed: () {},
+                  color: Colors.brown[300],
+                  textColor: Colors.white,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 200,
+                    alignment: Alignment.center,
+                    child: Text("Start Cooking", style: TextStyle(fontSize: 14)),
+                  ),
+                )
+              ],
+            ),
+          ) /* add child content here */,
+        )
+      ),
+    );
+  }
+}
